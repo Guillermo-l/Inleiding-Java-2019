4c9c2b3620d041f2475c0dfee89aed61f06181ea
Guillermo-l <guillermo.latumahina@student.rocvf.nl>
2019-09-02 16:39:10 +0200

    Hoofdstuk 4 opdrachten

---
+++ src/Hoofdstuk4/Opdracht1.java
@@ -0,0 +1,18 @@
+package Hoofdstuk4;
+
+    import java.applet.Applet;
+    import java.awt.*;
+
+public class Opdracht1 extends Applet {
+
+    public void init() {
+    }
+
+    public void paint(Graphics g) {
+        setBackground(Color.white);
+        g.setColor(Color.black);
+       g.drawLine(30,200,230,200);
+       g.drawLine(30,200,130,50);
+       g.drawLine(130,50,230,200);
+    }
+}
