bedbeee09f28b1917519a7c29f0e4f18b61ba557
Guillermo-l <guillermo.latumahina@student.rocvf.nl>
2019-09-02 15:37:50 +0200

    Hoofdstuk 2 opdrachten

---
+++ src/Hoofdstuk2/Opdracht1.java
@@ -0,0 +1,16 @@
+package Hoofdstuk2;
+
+    import java.applet.Applet;
+    import java.awt.*;
+
+public class Opdracht1 extends Applet {
+
+    public void init() {
+        setBackground(Color.blue);
+    }
+
+    public void paint(Graphics g) {
+        g.setColor(Color.yellow);
+        g.drawString("Guillermo Latumahina", 50, 60);
+    }
+}
