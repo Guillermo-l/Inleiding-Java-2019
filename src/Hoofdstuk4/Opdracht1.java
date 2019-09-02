ef06c377f18fc78d9d5778a2c7b836d933a09822
Guillermo-l <guillermo.latumahina@student.rocvf.nl>
2019-09-02 16:44:45 +0200

    Hoofdstuk 4 opdrachten

--- src/Hoofdstuk4/Opdracht1.java
+++ src/Hoofdstuk4/Opdracht1.java
@@ -1,7 +1,7 @@
 package Hoofdstuk4;
 
-    import java.applet.Applet;
-    import java.awt.*;
+import java.applet.Applet;
+import java.awt.*;
 
 public class Opdracht1 extends Applet {
 
@@ -11,8 +11,8 @@ public class Opdracht1 extends Applet {
     public void paint(Graphics g) {
         setBackground(Color.white);
         g.setColor(Color.black);
-       g.drawLine(30,200,230,200);
-       g.drawLine(30,200,130,50);
-       g.drawLine(130,50,230,200);
+        g.drawLine(30,200,230,200);
+        g.drawLine(30,200,130,50);
+        g.drawLine(130,50,230,200);
     }
 }
