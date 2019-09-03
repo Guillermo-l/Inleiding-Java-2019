fdae28506f52040d80b60319502597a2401364c9
Guillermo-l <guillermo.latumahina@student.rocvf.nl>
2019-09-03 08:48:28 +0200

    Hoofdstuk 4 opdrachten

---
+++ src/Hoofdstuk4/Opdracht6.java
@@ -0,0 +1,28 @@
+package Hoofdstuk4;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class Opdracht6 extends Applet {
+
+    public void init() {
+    }
+
+    public void paint(Graphics g) {
+        setBackground(Color.lightGray);
+
+        g.setColor(Color.black);
+        g.fillRect(50,50,80,210);
+        g.setColor(Color.red);
+        g.fillOval(65,65,50,50);
+        g.setColor(Color.orange);
+        g.fillOval(65,130,50,50);
+        g.setColor(Color.green);
+        g.fillOval(65,195,50,50);
+        g.setColor(Color.black);
+        g.fillRect(80,260,20,300);
+
+
+    }
+}
+
